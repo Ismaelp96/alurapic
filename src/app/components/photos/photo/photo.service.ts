95ec3e16f578d498f5af31f65f01901adb5a5aca
Ismael Oliveira <ismael6991@live.com>
2023-03-04 14:55:35 -0300

    Segmentos de rotas

--- src/app/components/photos/photo/photo.service.ts
+++ src/app/components/photos/photo/photo.service.ts
@@ -11,6 +11,6 @@ export class PhotoService {
   constructor(private http: HttpClient) {}
 
   listFromUser(userName: string) {
-    return this.http.get<Photo[]>(API + '/flavio/photos');
+    return this.http.get<Photo[]>(API + '/' + userName + '/photos');
   }
 }
