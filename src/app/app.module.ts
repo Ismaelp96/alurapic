712c897eb31709b0631ded546f964995defed317
Ismael Oliveira <ismael6991@live.com>
2023-03-04 13:01:54 -0300

    Diretiva  *ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
 
+import { AppComponent } from './app.component';
+import { PhotoComponent } from './components/photos/photo/photo.component';
 @NgModule({
   declarations: [AppComponent, PhotoComponent],
   imports: [BrowserModule, AppRoutingModule],
