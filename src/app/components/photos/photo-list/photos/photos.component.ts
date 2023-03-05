14cd7df0e47847fb3c1539b70bafe8c1df9eb8f5
Ismael Oliveira <ismael6991@live.com>
2023-03-05 11:44:47 -0300

    Implementando a lógica do grid

--- src/app/components/photos/photo-list/photos/photos.component.ts
+++ src/app/components/photos/photo-list/photos/photos.component.ts
@@ -8,8 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.rows = this.groupColumns(this.photos);
+  }
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
+  }
 }
