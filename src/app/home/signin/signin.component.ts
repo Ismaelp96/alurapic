760e20068306a3f54bc962599f628dd3a3febe6b
Ismael Oliveira <ismael6991@live.com>
2023-03-06 22:22:00 -0300

    viewChild: Obtendo referências do template

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -7,12 +9,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
   loginForm!: FormGroup;
+  @ViewChild('userNameInput') userNameInput!: ElementRef<HTMLInputElement>;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
-      currentPassword: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.authService.authenticate(userName, password).subscribe({
+      next: () => this.router.navigate(['user', userName]),
+      error: (err) => {
+        console.log(err);
+        this.loginForm.reset();
+        this.userNameInput.nativeElement.focus();
+      },
     });
   }
 }
