135627ec0b790b768966ba03bf2063ec05bcfee5
Ismael Oliveira <ismael6991@live.com>
2023-03-19 22:12:37 -0300

    Global error handler

--- src/app/errors/erros.module.ts
+++ src/app/errors/erros.module.ts
@@ -1,9 +1,16 @@
+import { GlobalErrorHandler } from './gobal-error-handler/global-error-handler';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [CommonModule],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler,
+    },
+  ],
 })
 export class ErrosModule {}
