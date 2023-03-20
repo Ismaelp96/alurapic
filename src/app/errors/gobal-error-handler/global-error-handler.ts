57c85a7ffb06f3e43baf3687a08e717dd0de281a
Ismael Oliveira <ismael6991@live.com>
2023-03-19 22:32:07 -0300

    Exibindo mais informações

--- src/app/errors/gobal-error-handler/global-error-handler.ts
+++ src/app/errors/gobal-error-handler/global-error-handler.ts
@@ -1,12 +1,32 @@
-import { ErrorHandler } from '@angular/core';
+import { UserService } from './../../core/user/user.service';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import * as StackTrace from 'stacktrace-js';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
+  constructor(private injector: Injector) {}
+
   handleError(error: any): void {
     console.log('passei pelo handler');
+
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+    const url = location instanceof PathLocationStrategy ? location.path() : '';
+
+    const message = error.message ? error.message : error.toString();
     StackTrace.fromError(error).then((stackFrames) => {
       const stackAsString = stackFrames.map((sf) => sf.toString()).join('\n');
+
+      console.log(message);
       console.log(stackAsString);
+      console.log('Abaixo é o que vai ser enviado para o servidor');
+      console.log({
+        message,
+        url,
+        userName: userService.getUserName(),
+        stack: stackAsString,
+      });
     });
   }
 }
